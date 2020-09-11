53d93ea90dca4a7aa5d5cb45c6935c4ab8c081e0
Anders Jakob Sivesind <ajsivesind@gmail.com>
2020-09-11 16:26:45 +0200

    Add project files.

---
+++ AI Service/Controllers/ProcessController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AI_Manager_Service.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AI_Manager_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        private readonly ILogger<ProcessController> _logger;
+
+        public ProcessController(ILogger<ProcessController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IEnumerable<string> Url(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> FileAsync(IFormFile file)
+        {
+            if (!Validation.NotNull(file) || !Validation.FileSize(file, 0, 5))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "File must be between 0 and 5 megabytes.");
+            }
+            else
+            {
+                string filePath = Path.GetTempFileName();
+
+                using (FileStream stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream).ConfigureAwait(false);
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AI Service/Utilities/Validation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AI_Manager_Service.Utilities
+{
+    public static class Validation
+    {
+        public static bool NotNull(Object value)
+        {
+            return value != null;
+        }
+
+        /// <summary>
+        /// Checks if the size of the provided file is at least minSize megabytes and at most maxSize megabytes.
+        /// </summary>
+        /// <param name="file">File to be validated</param>
+        /// <param name="minSize">Smallest allowed size in megabytes</param>
+        /// <param name="maxSize">Largest allowed size in megabytes</param>
+        /// <returns>True if the condition holds, false otherwise</returns>
+        public static bool FileSize(IFormFile file, long minSize, long maxSize)
+        {
+            Contract.Requires(file != null);
+            Contract.Requires(minSize <= maxSize);
+            Contract.Requires(minSize >= 0);
+
+            long minBytes = minSize * 1024 * 1024;
+            long maxBytes = maxSize * 1024 * 1024;
+
+            return file.Length >= minBytes && file.Length <= maxBytes;
+        }
+    }
+}
